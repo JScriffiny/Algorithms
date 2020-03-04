4b4727a71f49b161bdd954d7b14c44819025a8f7
JScriffiny <Jacob.Scriffiny@westpoint.edu>
2020-03-04 00:56:49 -0500

    Better tests

--- Problems/LongestSubstringNoRepeats/mySolution.scala
+++ Problems/LongestSubstringNoRepeats/mySolution.scala
@@ -38,13 +38,35 @@ object mySolution extends App {
     ("abrkaabcdefghijjxxx",10)
   )
 
-  println("#########   TESTING   #########")
+  implicit class ConsoleColorise(val str: String) extends AnyVal {
+    import Console._
+    def red    = s"$RED$str"
+    def green  = s"$GREEN$str"
+    def white  = s"$WHITE$str"
+    def cyan = s"$CYAN$str"
+  }
+
+  (1 to 60) foreach(_ => print("-"))
+  println("\n#########   TESTING   #########")
+  (1 to 60) foreach(_ => print("-"))
+  print("\n")
+  var testNum = 1
   for (test <- tests) {
+    println(("Test #" + testNum).cyan)
+    testNum += 1
+    print("".white)
     println(test._1)
     println("Expected: " + test._2)
     println("Received: " + uniqueRun(test._1))
-    println("-------------------")
+    if (test._2 == uniqueRun(test._1)) println("PASSED".green)
+    else println("**FAILED**".red)
+    print("".white)
+    print("\n")
+    (1 to 60) foreach(_ => print("-"))
+    print("\n")
   }
   val num = tests.count(test => uniqueRun(test._1) == test._2)
-  println("Passed " + num + "/" + tests.length + " tests")
+  println("#########   Passed " + num + "/" + tests.length + " tests   #########")
+  (1 to 60) foreach(_ => print("-"))
+  print("\n")
 }
